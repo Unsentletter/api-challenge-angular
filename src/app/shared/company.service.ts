0cd057ac483ea35f5519fe0b73a660d1d30b950e
Unsentletter <tristan.southwell@hotmail.com>
2017-01-31 11:53:01 +1100

    still struggling with headers

--- src/app/shared/company.service.ts
+++ src/app/shared/company.service.ts
@@ -9,11 +9,13 @@ export class CompanyService {
   ) {}
 
   getCompanies() {
-    let headers = new Headers({'Content-Type': 'application/json'});
+    let headers = new Headers({'Accept': 'application/json'});
     headers.append('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NGQxOTY4MGI1MWMxNTI2MGI5NDRmZDUiLCJpc3N1ZV9kYXRlIjoiMjAxNS0wOS0wOVQwNToxMzo1My40NThaIn0.Hk2XypA_KMUnIKdSVYnwq3Rn3QyMNSQ-e80-sZsA9bY')
-    let options = new RequestOptions({headers: headers});
+
+    let options = new RequestOptions({ headers: headers });
+
     return this.http.get(`http://api.demo.muulla.com/cms/merchant/all/active/10/1`, options)
-    .map((res:Response) => res.json());
+    .map(res => console.log(res));
   }
 
 }
