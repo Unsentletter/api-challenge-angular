01acfc5a6af2daeaa5deff47a191205048e6d7b6
Unsentletter <tristan.southwell@hotmail.com>
2017-01-31 11:50:24 +1100

    still struggling with headers

--- src/app/shared/company.service.ts
+++ src/app/shared/company.service.ts
@@ -9,11 +9,11 @@ export class CompanyService {
   ) {}
 
   getCompanies() {
-    var headers = new Headers({'Content-Type': 'application/json'});
-    headers.append('authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NGQxOTY4MGI1MWMxNTI2MGI5NDRmZDUiLCJpc3N1ZV9kYXRlIjoiMjAxNS0wOS0wOVQwNToxMzo1My40NThaIn0.Hk2XypA_KMUnIKdSVYnwq3Rn3QyMNSQ-e80-sZsA9bY');
-    var options = new RequestOptions({headers: headers});
-
-    return this.http.get(`http://api.demo.muulla.com/cms/merchant/all/active/10/1`,options)
+    let headers = new Headers({'Content-Type': 'application/json'});
+    headers.append('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NGQxOTY4MGI1MWMxNTI2MGI5NDRmZDUiLCJpc3N1ZV9kYXRlIjoiMjAxNS0wOS0wOVQwNToxMzo1My40NThaIn0.Hk2XypA_KMUnIKdSVYnwq3Rn3QyMNSQ-e80-sZsA9bY')
+    let options = new RequestOptions({headers: headers});
+    return this.http.get(`http://api.demo.muulla.com/cms/merchant/all/active/10/1`, options)
     .map((res:Response) => res.json());
   }
+
 }
